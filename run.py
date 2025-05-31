e52a19ee6e402158239a5b77e5879121eee9ca3b
WojBor87 <w.borawski@cotynatostudio.pl>
2025-05-31 21:06:44 +0200

    Trash removed

--- run.py
+++ run.py
@@ -2,10 +2,6 @@ from app import create_app
 
 app = create_app()
 
-app.route('/')
-def index():
-    return "Welcome to the Flask Application!" 
-
 if __name__ == '__main__':
     app.run(debug=True)
 
